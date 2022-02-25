b40300dfb7af089a5a68074fe99fc0d6c5dfa166
Krishna <krishna.vamshi@yumzy.in>
2022-02-25 19:57:35 +0530

    added outer array details(out of ecommerce)

--- index.js
+++ index.js
@@ -10,8 +10,6 @@ const productRoute = require("./routes/product");
 const cartRoute = require("./routes/cart");
 const orderRoute = require("./routes/order");
 const stripeRoute = require("./routes/stripe");
-
-const testRouteOuter = require("./routes/outer-users");
 const cors = require("cors");
 
 dotenv.config();
@@ -32,7 +30,6 @@ app.use("/api/products", productRoute);
 app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
 app.use("/api/checkout", stripeRoute);
-app.use("/api/testRoute", testRouteOuter);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("server is running");
