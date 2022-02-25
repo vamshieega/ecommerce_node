602a0b93e41cf602caef818def8a9badb1916601
vamshieega <75155110+vamshieega@users.noreply.github.com>
2022-02-25 19:33:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const productRoute = require("./routes/product");
 const cartRoute = require("./routes/cart");
 const orderRoute = require("./routes/order");
 const stripeRoute = require("./routes/stripe");
-const cors = require("cors");
 
 dotenv.config();
 mongoose
@@ -20,7 +19,6 @@ mongoose
     console.log(err);
   });
 
-app.use(cors());
 app.use(express.json());
 app.use("/api/users", userRoute);
 
